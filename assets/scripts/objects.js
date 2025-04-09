891b4cf99f2289fd6af8a11dbdfe6f1b41fda5ff
ShaQay3Q <shaghay3gh@gmail.com>
2025-04-09 21:21:21 +0200

    handle search on renderMovies()

--- assets/scripts/objects.js
+++ assets/scripts/objects.js
@@ -15,18 +15,23 @@ const clearInputs = () => {
 
 const movies = [];
 
-const renderMovies = (movieArr) => {
+const renderMovies = (filter = "") => {
 	const movieList = document.getElementById("movie-list");
 	if (!movieList) return; // if element is not found
 
 	// movies.length === 0
 	// 	? movieList.classList.remove("visible")
 	// 	: movieList.classList.add("visible");
-	movieList.classList.toggle("visible", movieArr.length > 0);
+	movieList.classList.toggle("visible", movies.length > 0);
 	movieList.innerHTML = "";
 
 	const fragment = document.createDocumentFragment(); // Create a temporary container
-	movieArr.forEach((movie) => {
+
+	const filteredMovies = !filter
+		? movies
+		: movies.filter((movie) => movie.info.movieTitle.includes(filter));
+
+	filteredMovies.forEach((movie) => {
 		const movieElement = document.createElement("li");
 		// let text = movie.info.movieTitle + " - " + movie.info[extraName.value];
 		// movieElement.textContent = text;
@@ -60,7 +65,7 @@ const addMovieHandler = () => {
 		id: Math.random(),
 	};
 	movies.push(newMovie);
-	renderMovies(movies);
+	renderMovies();
 
 	// Reset the fields
 	clearInputs();
@@ -68,33 +73,39 @@ const addMovieHandler = () => {
 
 const searchMovieHandler = () => {
 	// Validate input data
-	const searchText = filterInput.value.trim().toLowerCase();
-	if (searchText === "") {
-		renderMovies(movies); // Show all movies if search is empty
-		return;
-	}
+	const searchText = filterInput.value.trim();
+	console.log(searchText);
 
-	let founds = movies.filter((movie) => {
-		// if (movie.info.movieTitle.toLowerCase().includes(searchText)) {
-		// 	return true;
-		// }
+	// if (searchText === "") {
+	// 	renderMovies(movies); // Show all movies if search is empty
+	// 	return;
+	// }
 
-		// Check all extra properties
-		for (let key in movie.info) {
-			if (
-				key === movie.info.movieTitle.toLowerCase().includes(searchText) ||
-				key === searchText ||
-				movie.info[key].toLowerCase().includes(searchText)
-			) {
-				return true;
-			}
-		}
+	// let founds = movies.filter((movie) => {
+	// 	console.log(movie);
 
-		return false;
-	});
-	console.log(founds);
+	// 	// return movie.includes(searchText);
+	// 	// if (movie.info.movieTitle.toLowerCase().includes(searchText)) {
+	// 	// 	return true;
+	// 	// }
 
-	renderMovies(founds);
+	// 	// Check all extra properties
+	// 	for (let key in movie.info) {
+	// 		if (
+	// 			key === movie.info.movieTitle.toLowerCase().includes(searchText) ||
+	// 			key === searchText ||
+	// 			movie.info[key].toLowerCase().includes(searchText)
+	// 		) {
+	// 			return true;
+	// 		}
+	// 	}
+
+	// 	return false;
+	// });
+	// console.log(founds);
+
+	// renderMovies(founds);
+	renderMovies(searchText);
 };
 
 // Ensure inputs are cleared when the page loads
